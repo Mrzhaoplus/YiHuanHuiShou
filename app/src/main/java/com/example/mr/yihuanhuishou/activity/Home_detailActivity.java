85d93b507c44b5396305e60cd12021bdc31672ce
[Mrzhaoplus] <[919062838@qq.com]>
2018-05-02 14:26:28 +0800

    zwb5/2

---
+++ app/src/main/java/com/example/mr/yihuanhuishou/activity/Home_detailActivity.java
@@ -0,0 +1,15 @@
+package com.example.mr.yihuanhuishou.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mr.yihuanhuishou.R;
+
+public class Home_detailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_detail);
+    }
+}
--- app/src/main/java/com/example/mr/yihuanhuishou/activity/MainActivity.java
+++ app/src/main/java/com/example/mr/yihuanhuishou/activity/MainActivity.java
@@ -1,21 +1,26 @@
-package com.example.mr.yihuanhuishou;
+package com.example.mr.yihuanhuishou.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.example.mr.yihuanhuishou.R;
 import com.example.mr.yihuanhuishou.base.BaseActivity;
 import com.example.mr.yihuanhuishou.utils.MyContants;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
-
+      @BindView(R.id.huishou_img)
+      ImageView huishou_img;
+      @BindView(R.id.siji_img)
+      ImageView siji_img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MyContants.windows(this);
         ButterKnife.bind(this);
-
     }
 }
